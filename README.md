41d3fd5cb4dd4334d74f49dd584fa54301ea365b
Dmitry Gamanenko <dmitry.gamanenko@raccoongang.com>
2016-10-24 11:58:40 +0300

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+#To Update COMPREHENSIVE THEME run script:
+
+git clone https://github.com/raccoongang/themex_script.git
+
+cd update-comprehensive-theme/
+
+./update_theme.sh theme_branch_name
+
---
+++ update_theme.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+    echo "theme is unset: use --> ./update_theme.sh theme_branch"
+else
+    ansible-playbook -c local -i "localhost," prepare.yml
+    if [ -z "$2" ]
+	then
+	    /tmp/edxthemevenv/bin/ansible-playbook -c local -i "localhost," main.yml -e theme_branch="\'$1\'"
+	else
+		/tmp/edxthemevenv/bin/ansible-playbook -c local -i "localhost," main.yml -e theme_branch="\'$1\'" -e theme_repo="\'$2\'"
+	fi
+
+fi
+
