5a4138cceb4960476f8607d596e76a21520da7bd
ubuntu <ubuntu@themexupd.raccoongang.com>
2016-11-23 18:51:27 +0200

    split playbook into roles, add local and remote zip-theme-file support

--- README.md
+++ README.md
@@ -1,13 +1,72 @@
-#To update a Comprehensive Theme run the script:
+##OpenEdx Comprehensive theme autoinstall
 
-- git clone https://github.com/raccoongang/themex_script.git
+###QUICK START:
+###REQUIREMENTS:
+#####Project tested with:
+```
+ansible >=2.0.1.0
+host: Ubuntu 12.04
+```
 
-- cd themex_script
+## Clone repo:
+```
+git clone https://github.com/raccoongang/themex_script.git
+```
+##Run script
+```
+cd themex_script
+```
 
-- ./update_theme.sh marvel-yellow-theme-eucalyptus ​
+### use local zip theme file:  
+```
+./update_theme.sh -l /some/local/zip-file/path/theme-name.zip
+```
 
-marvel-yellow-theme-eucalyptus - is the name of the theme branch taken from our gitHub [repository](https://github.com/raccoongang/themes_for_themex.io/branches) with themes. We have named our branches according to what Open edX version the theme should be installed to. 
+###use remote zip theme file: 
+```
+./update_theme.sh -r http://themex.io/...../theme-name.zip
+```
+
+###use theme branch in raccoongan repo (download theme from raccoongang repo with specific branch): 
+```
+./update_theme.sh -b theme_branch
+```
+
+###theme branch in custom repo (download theme from specific repo with specific branch): 
+```
+./update_theme.sh -br theme_branch theme_repo
+```
+
+###setup theme branch in custom repo into some edx host:
+```
+./update_theme.sh -brh theme_branch theme_repo edx_host
+```
+####example:
+```
+./update_theme.sh -brh marvel-theme-eucalyptus https://github.com/raccoongang/themes_for_themex.io.git 200.83.1.109
+```
 
 
-More detailed instruction is available [here](https://themex.io/install/)
+###setup theme branch in custom repo into some group of edx hosts (!you need to modify inventory.ini file!): 
+```
+./update_theme.sh -brhi theme_branch theme_repo edx_host_group
+```
+####example:
+```
+./update_theme.sh -brhi marvel-yellow-theme-eucalyptus https://github.com/raccoongang/themes_for_themex.io.git edx
+
+inventory.ini file contained (in this case)
+
+[edx]
+200.83.1.109
+```
+
+#### If you use some configuration server for setup theme on remote Edx instance you need to install ansible locally
+
+```
+sudo apt-get -y install software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get -y install ansible
+```
 
--- update_theme.sh
+++ update_theme.sh
@@ -1,18 +1,64 @@
 #!/bin/bash
 
-if [ -z "$1" ]
+INI_FILE='inventory.ini'
+
+mode_local_zip='-l'
+mode_remote_zip='-r'
+mode_branch_rg='-b'
+mode_branch_custom_repo='-br'
+mode_branch_custom_repo_remote_edx_host='-brh'
+mode_branch_custom_repo_remote_group_edx_host='-brhi'
+
+if test "$#" -le 1;
 then
-    echo "theme is unset: use --> ./update_theme.sh theme_branch"
+    echo ""
+    echo "theme is unset! use one of correct correct script-run variant:"
+    echo ""
+    echo "local zip file:  ./update_theme.sh $mode_local_zip /some/local/zip-file/path/theme-name.zip"
+    echo ""
+    echo "remote zip file: ./update_theme.sh $mode_remote_zip http://themex.io/...../theme-name.zip"
+    echo ""
+    echo "theme branch in raccoongan repo: ./update_theme.sh $mode_branch_rg theme_branch"
+    echo ""
+    echo "theme branch in custom repo: ./update_theme.sh $mode_branch_custom_repo theme_branch theme_repo"
+    echo ""
+    echo "setup theme branch in custom repo into some edx host: ./update_theme.sh $mode_branch_custom_repo_remote_edx_host theme_branch theme_repo edx_host"
+    echo ""
+    echo "setup theme branch in custom repo into some group of edx hosts: ./update_theme.sh $mode_branch_custom_repo_remote_group_edx_host theme_branch theme_repo edx_host_group (!you need to modify inventory.ini file!)"
+    echo ""
 else
-    ansible-playbook -c local -i "localhost," prepare.yml
-    if [ -z "$2" ]
-    then
-        ~/.edxthemevenv/bin/ansible-playbook -c local -i "localhost," main.yml -e "theme_branch=$1"
-    elif [ -z "$3" ]
-    then
-        ~/.edxthemevenv/bin/ansible-playbook -c local -i "localhost," main.yml -e "theme_branch=$1 theme_repo=$2"
-    else
-        ~/.edxthemevenv/bin/ansible-playbook -i inventory.ini main.yml -e "theme_branch=$1 theme_repo=$2 edx_hosts=$3"
-    fi
+
+	ansible-playbook -c local -i "localhost," prepare.yml
+
+	if test "$#" -eq 2;
+	then
+		if [ "$1" == "$mode_local_zip" ];
+		then
+			~/.edxthemevenv/bin/ansible-playbook -c local -i ",localhost" main.yml -e "mode=${1//[-]/} local_zip=$2"
+		elif [ "$1" == "$mode_remote_zip" ];
+		then
+			~/.edxthemevenv/bin/ansible-playbook -c local -i ",localhost" main.yml -e "mode=${1//[-]/} remote_zip=$2"
+		elif [ "$1" == "$mode_branch_rg" ];
+		then
+			~/.edxthemevenv/bin/ansible-playbook -c local -i ",localhost" main.yml -e "mode=${1//[-]/} theme_branch=$2"
+		fi
+
+	elif test "$#" -eq 3;
+	then
+		if [ "$1" == "$mode_branch_custom_repo" ];
+		then
+			~/.edxthemevenv/bin/ansible-playbook -c local -i ",localhost" main.yml -e "mode=${1//[-]/}" -e "theme_branch=$2 theme_repo=$3"
+		fi
+	elif test "$#" -eq 4;
+	then
+		if [ "$1" == "$mode_branch_custom_repo_remote_edx_host" ];
+		then
+			~/.edxthemevenv/bin/ansible-playbook -i ",$4" main.yml -e "edx_hosts=$4" -e "mode=${1//[-]/}" -e "theme_branch=$2 theme_repo=$3"
+        elif [ "$1" == "$mode_branch_custom_repo_remote_group_edx_host" ];
+        then
+        	~/.edxthemevenv/bin/ansible-playbook -i $INI_FILE main.yml --limit $4 -e "edx_hosts=$4" -e "mode=${1//[-]/}" -e "theme_branch=$2 theme_repo=$3"
+		fi
+	fi
 
 fi
+
